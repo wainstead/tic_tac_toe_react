5fba6d34d9aeed2412fe4b32cbe425301e5e17b2
Steve Wainstead <swain@panix.com>
2018-11-06 17:04:38 -0500

    Refactoring: put helper classes in their own files/folders

--- src/index.js
+++ src/index.js
@@ -1,68 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-// class Square extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             value: null,
-//         };
-//     }
-//     render() {
-//       return (
-//         <button 
-//         className="square" 
-//         onClick={() => this.props.onClick()}
-//         >
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
-
-function Square(props) {
-    return (
-        <button 
-        className="square" 
-        onClick={props.onClick
-        }>
-        {props.value}
-        </button>
-    )
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-        <Square 
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)} />
-        );
-    }
-
-    render() {
-        return (
-        <div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
-        );
-    }
-}
+import Board from './Board/Board';
 
 class Game extends React.Component {
     constructor(props) {
